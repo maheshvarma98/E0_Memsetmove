72b9c7020c685e1b7eb1ffb3d7714f1993fc9fcf
maheshvarma98 <dommaraju.8@wright.edu>
2023-05-18 01:03:14 -0400

    Added my comments

--- src/memoryoperations.cpp
+++ src/memoryoperations.cpp
@@ -7,6 +7,8 @@ using namespace std;
 Pet* MemoryOperations::memmove(
     Pet* destination_p, const Pet* source_p, size_t count)
 {
+    //Comparing destination address with source address (since we know it is a contiguous address)
+    //if it is greater we copy in reverse order else we copy in original order
     if (destination_p > source_p) {
         for (int32_t i = count - 1; i >= 0; --i) {
             destination_p[i] = source_p[i];

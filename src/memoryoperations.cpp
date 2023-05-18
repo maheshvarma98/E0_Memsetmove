b73e148bb30237701100f24988fa92cfd544fd16
maheshvarma98 <dommaraju.8@wright.edu>
2023-05-18 00:52:47 -0400

    First Commit

--- src/memoryoperations.cpp
+++ src/memoryoperations.cpp
@@ -7,8 +7,6 @@ using namespace std;
 Pet* MemoryOperations::memmove(
     Pet* destination_p, const Pet* source_p, size_t count)
 {
-    const Pet* sourceEnd_p = source_p + count;
-
     if (destination_p > source_p) {
         for (int32_t i = count - 1; i >= 0; --i) {
             destination_p[i] = source_p[i];
